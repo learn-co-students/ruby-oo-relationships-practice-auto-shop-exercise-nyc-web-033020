0309150deb844ae9981a6a695456420585a0c70c
mishyjari <mishyjari@outlook.com>
2020-04-05 11:32:57 -0400

    all deliverables met

--- app/models/car_owner.rb
+++ app/models/car_owner.rb
@@ -1,9 +1,28 @@
 class CarOwner
-
+  @@all = []
   attr_reader :name
 
   def initialize(name)
     @name = name
+    CarOwner.all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def cars
+    Car.all.select{ |car| car.owner == self }
+  end
+
+  def mechanics
+    Car.all.select{ |car| car.owner == self }
+      .map{ |car| car.mech }
+      .uniq
+  end
+
+  def self.avg_num_cars
+    CarOwner.all.map{ |owner| owner.cars.length.to_f }.sum / CarOwner.all.length
   end
 
 
